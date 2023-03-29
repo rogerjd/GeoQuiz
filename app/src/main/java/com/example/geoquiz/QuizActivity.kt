62d5ce64f4830206513ddee13f6ec347952ac40f
roger <rjpd@netzero.com>
2023-03-28 20:36:37 -0400

    init

---
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -0,0 +1,15 @@
+package com.example.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class QuizActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
